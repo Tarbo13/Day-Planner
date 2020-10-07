8eb3144ef7f7516769d0cb45a96330da8ee9d91f
Ben Robinson <robinson.dri@gmail.com>
2020-10-06 18:02:07 -0700

    redoing local storage

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Day-Planner
+# Project Discription
+
+Here I was tasked with creating a day planner app that would display and track the current time and day.  My app consists of nine hour blocks of time that will be shadded a diffrent color based on the time of day: (Grey == Past, Red == Present, & Green == Future).  A user is able to enter their daily tasks into each hour block and save the entries so that they remain on the page after it is refreshed or closed and reopened.  
+I used a combination of Bootstrap, JavaScript, JQuery, and CSS to create this app.  Bootstrap's grid system is used to create the layout of the app.  I then used varous CSS classes to style the page.  I also used FontAwesome in this project to get the disk icons that were used on the save buttons.  
+
+
+## Links
+
+[GitHub Repository](https://github.com/Tarbo13/Day-Planner)
+
+<br>
+
+[Deployed Web Page](https://tarbo13.github.io/Day-Planner/)
+
+### Screen Shots
+
+![Time Block Shadding](https://user-images.githubusercontent.com/68627417/95254126-478fce00-07d4-11eb-8777-af95044fc097.png)
+
+<br>
+
+![Saved User Input in Time Blocks](https://user-images.githubusercontent.com/68627417/95275407-41621780-07fd-11eb-9306-6e6d4e99db06.png)
+
+
--- script.js
+++ script.js
@@ -3,6 +3,17 @@
 
 $(document).ready(function () {
 
+	// create variables to store user input
+	var text9AMEl = $("#9AM");
+	var text10AMEl = $("#10AM");
+	var text11AMEl = $("#11AM");
+	var text12AMEl = $("#12AM");
+	var text13AMEl = $("#1PM");
+	var text14AMEl = $("#2PM");
+	var text15AMEl = $("#3PM");
+	var text16AMEl = $("#4PM");
+	var text17AMEl = $("#5PM");
+
 	// create variable to log current hour
 	var currentHour = moment().hour();
 	console.log(currentHour);
@@ -92,63 +103,44 @@ $(document).ready(function () {
 		$("#hour-17").addClass("future");
 	}	
 	
-	// crate local storage to save user input for each hour
+	// crate local storage to save user input for each hour after save button is clicked.
 
 	
-	// var entryEl = document.querySelector("#entry");
 
 	$(".saveBtn").click(function(event){
 		event.preventDefault();    
 	  
-		var value = $(this).siblings(".description").val();
-		console.log('The value of the text is:', value);
-				
+	localStorage.setItem("input9", (text9AMEl.val()));
+	localStorage.setItem("input10", text10AMEl.val());
+	localStorage.setItem("input11", text11AMEl.val());
+	localStorage.setItem("input12", text12AMEl.val());
+	localStorage.setItem("input13", text13AMEl.val());
+	localStorage.setItem("input14", text14AMEl.val());
+	localStorage.setItem("input15", text15AMEl.val());
+	localStorage.setItem("input16", text16AMEl.val());
+	localStorage.setItem("input17", text17AMEl.val());
 		
-		$(".row").each(function(){
-			var key = $(this).attr("id");
-			localStorage.getItem(key);
-			console.log('The key (hour) is: ', key);
-
-			
-			
-
-			localStorage.setItem(key, value);
-			$("<textarea>").text(value);
-
-					
 	
-		  });
-		  
-	});
-	
+	});	
 
-	 
-	$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-	$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-	$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-	$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-	$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-	$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-	$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-	$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-	$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
+	// append loacal storage to HTML
 	
+	$("#9AM").append(localStorage.getItem("input9"));
+	$("#10AM").append(localStorage.getItem("input10"));
+	$("#11AM").append(localStorage.getItem("input11"));
+	$("#12AM").append(localStorage.getItem("input12"));
+	$("#1PM").append(localStorage.getItem("input13"));
+	$("#2PM").append(localStorage.getItem("input14"));
+	$("#3PM").append(localStorage.getItem("input15"));
+	$("#4PM").append(localStorage.getItem("input16"));
+	$("#5PM").append(localStorage.getItem("input17"));
 
 });
 
 
 
-// //1. store array values in the local stoorage for each hour
-// //2. first get the values from the localstorage and save it in a var
-// var datafromLoca = localStorage.getItem("hour-9");
 
-// //userData is the user text
-// datafromLoca.push(userData);
 
-// //now store the datafromLoca to the localstorage
-// localStorage.setItem("hour-9", datafromLoca);
-// localStorage.setItem("hour-10", datafromLoca);
 
 
 
